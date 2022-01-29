af091a702c27889232556bb58480717dff86c810
kamil.kuchaev <kamil.kuchaev@analyticum.pro>
2022-01-29 10:51:49 +0500

    commit homework lesson 5

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers } from 'redux'
+import { profileReducer } from './profile'
+import {chatReducer} from "./chat";
+import {messageReducer} from "./message";
+
+export const store = createStore(
+  combineReducers({
+    profile: profileReducer,
+    chat: chatReducer,
+    message: messageReducer,
+    }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
---
+++ src/store/profile/actions.js
@@ -0,0 +1,6 @@
+export const TOGGLE_SHOW_PROFILE = 'TOGGLE_SHOW_PROFILE'
+
+export const toggleShowProfileAction = (isToggle) => ({
+  type: TOGGLE_SHOW_PROFILE,
+  payload: isToggle
+})
